aac68f8a485a7f7c5989c4c1732e6863aaf16240
javadjh <hojati.jh@gmail.com>
2022-11-26 15:49:23 +0330

    fix big

--- backend/src/config/globalUtility.ts
+++ backend/src/config/globalUtility.ts
@@ -28,7 +28,7 @@ export async function downloadImage(url: string) {
    console.log(url.substring(url.lastIndexOf(".") + 1, url.length ));
    console.log(url.substring(url.lastIndexOf(".") + 1, url.length +1));
    
-   let ex = url.substring(url.lastIndexOf(".") + 1, url.length - 1);
+   let ex = url.substring(url.lastIndexOf(".") + 1, url.length);
    let filename = await download.image({
       url,
       dest: getDist() + `/${Date.now()}.${ex}`,
