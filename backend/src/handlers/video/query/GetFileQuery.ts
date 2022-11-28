b52a8af89c59a9ba26e336d476576f388853c738
javadjh <hojati.jh@gmail.com>
2022-11-28 14:45:19 +0330

    change the quality

--- backend/src/handlers/video/query/GetFileQuery.ts
+++ backend/src/handlers/video/query/GetFileQuery.ts
@@ -43,7 +43,7 @@ const getFile = async (format: videoFormat, videoData: any, res,req:any) => {
    let { fileName, urlFileName } = getFileName(format);
 
    const video = ytdl(videoData.url,{
-      quality:'134'
+      quality:'136'
    });
    let starttime;
    video.pipe(fs.createWriteStream(fileName));
