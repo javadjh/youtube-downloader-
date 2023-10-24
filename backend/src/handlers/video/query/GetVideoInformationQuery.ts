a94e25701c66e9df0f6b595ea4d25a75ecc2c6be
javad <javad>
2023-10-24 13:02:24 +0330

    fix

--- backend/src/handlers/video/query/GetVideoInformationQuery.ts
+++ backend/src/handlers/video/query/GetVideoInformationQuery.ts
@@ -120,7 +120,7 @@ const getVideoInformation = middleware(
                video.youtubeFileLink = data;
                let dis = await downloadImage(video.image);
                video.image =
-                  'http://dl.lifelands.ir/video/' +
+                  'http://5.75.132.228:5500/upload/' +
                   dis.substring(dis.lastIndexOf('/') + 1, dis.length);
 
                if (video.image.includes('?')) {
@@ -132,7 +132,7 @@ const getVideoInformation = middleware(
                      dis.substring(dis.lastIndexOf('/') + 1, dis.indexOf('?'))
                   );
                   video.image =
-                     'http://dl.lifelands.ir/video/' +
+                     'http://5.75.132.228:5500/upload/' +
                      dis.substring(dis.lastIndexOf('/') + 1, dis.indexOf('?'));
                }
                console.log(video.image);
