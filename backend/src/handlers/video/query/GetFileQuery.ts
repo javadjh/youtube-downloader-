322a43faa6b2c735381b6891fe0c872f1f2eaf68
javad <48103478+javadjh@users.noreply.github.com>
2023-12-08 14:39:31 +0330

    fix

--- backend/src/handlers/video/query/GetFileQuery.ts
+++ backend/src/handlers/video/query/GetFileQuery.ts
@@ -85,8 +85,8 @@ const getFile = async (format: videoFormat, videoData: any, res, req: any) => {
       );
 
       socket?.emit('downloadYoutubeProgress', {
-         userId: req?.headers?.userId || 'test',
-         link: req.params?.link,
+         userId: req?.query?.userId || 'test',
+         link: req.query?.link,
          progress: `زمان حدودی باقی مانده ${downloadedMinutes.toFixed(
             2
          )} / حجم : (${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(
