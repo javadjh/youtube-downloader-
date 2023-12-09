13c160074b78f2652c9437b362c6181f3103e121
javad <48103478+javadjh@users.noreply.github.com>
2023-12-09 14:30:13 +0330

    fix

--- backend/src/handlers/video/query/GetFileQuery.ts
+++ backend/src/handlers/video/query/GetFileQuery.ts
@@ -43,86 +43,93 @@ export const getFileQuery = middleware(
 
 const getFile = async (format: videoFormat, videoData: any, res, req: any) => {
    let { fileName, urlFileName, name } = await getFileName(format);
-
-   socket?.emit('linkStep', {
-      userId: req?.query?.userId || 'test',
-      link: req.query?.link,
-      step: 'در حال دریافت فایل از یوتوب',
-   });
-   const video = ytdl(videoData.url, {
-      filter: 'videoandaudio',
-   });
-   let starttime;
-   video.pipe(fs.createWriteStream(fileName));
-   video.once('response', () => {
-      starttime = Date.now();
-   });
-   video.on('progress', (chunkLength, downloaded, total) => {
-      const percent = downloaded / total;
-      const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
-      const estimatedDownloadTime =
-         downloadedMinutes / percent - downloadedMinutes;
-      readline.cursorTo(process.stdout, 0);
-      process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
-      process.stdout.write(
-         `(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(
-            total /
-            1024 /
-            1024
-         ).toFixed(2)}MB)\n`
-      );
-      process.stdout.write(
-         `running for: ${downloadedMinutes.toFixed(2)}minutes`
-      );
-      process.stdout.write(
-         `, estimated time left: ${estimatedDownloadTime.toFixed(2)}minutes `
-      );
-
-      //connect to socket
-
-      socket?.emit('downloadYoutubeProgress', {
-         userId: req?.query?.userId || 'test',
-         link: req.query?.link,
-         progress: `زمان حدودی باقی مانده ${downloadedMinutes.toFixed(
-            2
-         )} / حجم : (${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(
-            total /
-            1024 /
-            1024
-         ).toFixed(2)}MB) -> ${(percent * 100).toFixed(2)}%`,
-      });
-
-      //write in res download prossess
-      let progress = 0;
-      const file_size = req.headers['content-length'];
-
-      // set event listener
-      req.on('data', (chunk) => {
-         progress += chunk.length;
-         const percentage = (progress / file_size) * 100;
-         console.log(percentage);
-      });
-
-      readline.moveCursor(process.stdout, 0, -1);
-   });
-   video.on('end', async () => {
-      process.stdout.write('\n\n');
-      videoData.files.push({
-         itag: format.itag,
-         file: urlFileName,
-      });
-      await videoData.save();
-
+   try {
       socket?.emit('linkStep', {
          userId: req?.query?.userId || 'test',
          link: req.query?.link,
-         step: 'در حال انتقال فایل از سرور به سرور دانلود',
+         step: 'در حال دریافت فایل از یوتوب',
       });
+      const video = ytdl(videoData.url, {
+         filter: 'videoandaudio',
+      });
+      let starttime;
+      video.pipe(fs.createWriteStream(fileName));
+      video.once('response', () => {
+         starttime = Date.now();
+      });
+      video.on('progress', (chunkLength, downloaded, total) => {
+         const percent = downloaded / total;
+         const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
+         const estimatedDownloadTime =
+            downloadedMinutes / percent - downloadedMinutes;
+         readline.cursorTo(process.stdout, 0);
+         process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
+         process.stdout.write(
+            `(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(
+               total /
+               1024 /
+               1024
+            ).toFixed(2)}MB)\n`
+         );
+         process.stdout.write(
+            `running for: ${downloadedMinutes.toFixed(2)}minutes`
+         );
+         process.stdout.write(
+            `, estimated time left: ${estimatedDownloadTime.toFixed(2)}minutes `
+         );
 
-      await FTPUploadFile(name, 'video');
+         //connect to socket
 
-      return res.status(200).send(urlFileName).end();
-   });
+         socket?.emit('downloadYoutubeProgress', {
+            userId: req?.query?.userId || 'test',
+            link: req.query?.link,
+            progress: `زمان حدودی باقی مانده ${downloadedMinutes.toFixed(
+               2
+            )} / حجم : (${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(
+               total /
+               1024 /
+               1024
+            ).toFixed(2)}MB) -> ${(percent * 100).toFixed(2)}%`,
+         });
+
+         //write in res download prossess
+         let progress = 0;
+         const file_size = req.headers['content-length'];
+
+         // set event listener
+         req.on('data', (chunk) => {
+            progress += chunk.length;
+            const percentage = (progress / file_size) * 100;
+            console.log(percentage);
+         });
+
+         readline.moveCursor(process.stdout, 0, -1);
+      });
+      video.on('end', async () => {
+         process.stdout.write('\n\n');
+         videoData.files.push({
+            itag: format.itag,
+            file: urlFileName,
+         });
+         await videoData.save();
+
+         socket?.emit('linkStep', {
+            userId: req?.query?.userId || 'test',
+            link: req.query?.link,
+            step: 'در حال انتقال فایل از سرور به سرور دانلود',
+         });
+
+         await FTPUploadFile(name, 'video');
+         return res.status(200).send(urlFileName).end();
+      });
+   } catch (error) {
+      socket?.emit('linkStep', {
+         userId: req?.query?.userId || 'test',
+         link: req.query?.link,
+         step: 'لینک مشکل دارد',
+      });
+      return res.status(400).send({}).end();
+   }
 };
 const getFileName = async (format: videoFormat): Promise<any> => {
    let mime = format.mimeType;
