f542118d4add0f1615364f8981a072d0a783db7c
mohmmdsjjd <salehimohammadsajad@gmail.com>
2024-02-05 17:50:28 +0330

    fix

--- backend/src/handlers/video/command/InsertVideoCommand.ts
+++ backend/src/handlers/video/command/InsertVideoCommand.ts
@@ -3,6 +3,6 @@ import { IVideo } from 'interfaces/video';
 
 export const insertVideo = async (data: IVideo): Promise<IVideo> => {
    const video: IVideo = await new VideoSchema(data).save();
-   let findVideo = await await VideoSchema.findById(video._id).lean();
+   let findVideo = await VideoSchema.findById(video._id).lean();
    return findVideo;
 };
